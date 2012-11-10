912261e8b55d01a94a7f6085b2b773905e2ba24e
Igor Fedorenko <igor@ifedorenko.com>
2012-11-10 12:52:49 -0500

    first commit
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

---
+++ com.ifedorenko.m2e.nexusdev/src/com/ifedorenko/m2e/nexusdev/internal/NexusExternalLaunchDelegate.java
@@ -0,0 +1,9 @@
+package com.ifedorenko.m2e.nexusdev.internal;
+
+import org.eclipse.jdt.launching.JavaLaunchDelegate;
+
+public class NexusExternalLaunchDelegate
+    extends JavaLaunchDelegate
+{
+
+}
