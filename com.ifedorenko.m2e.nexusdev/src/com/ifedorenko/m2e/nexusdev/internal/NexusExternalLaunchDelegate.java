7a41a4bab1fcb877df0f62fe6866e6aa0b114f8c
Igor Fedorenko <igor@ifedorenko.com>
2012-11-10 13:28:42 -0500

    launch hardcoded nexus instance
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- com.ifedorenko.m2e.nexusdev/src/com/ifedorenko/m2e/nexusdev/internal/NexusExternalLaunchDelegate.java
+++ com.ifedorenko.m2e.nexusdev/src/com/ifedorenko/m2e/nexusdev/internal/NexusExternalLaunchDelegate.java
@@ -1,9 +1,72 @@
 package com.ifedorenko.m2e.nexusdev.internal;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.codehaus.plexus.util.DirectoryScanner;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.jdt.launching.JavaLaunchDelegate;
 
 public class NexusExternalLaunchDelegate
     extends JavaLaunchDelegate
 {
+    File basedir = new File( "/opt/nexus/nexus-professional-2.2-01" );
 
+    @Override
+    public File verifyWorkingDirectory( ILaunchConfiguration configuration )
+        throws CoreException
+    {
+        return basedir;
+    }
+
+    @Override
+    public String verifyMainTypeName( ILaunchConfiguration configuration )
+        throws CoreException
+    {
+        return "org.sonatype.nexus.bootstrap.jsw.JswLauncher";
+    }
+
+    @Override
+    public String[] getClasspath( ILaunchConfiguration configuration )
+        throws CoreException
+    {
+
+        DirectoryScanner ds = new DirectoryScanner();
+        ds.setBasedir( basedir );
+        ds.setIncludes( new String[] { "bin/jsw/lib/wrapper-*.jar", "lib/*.jar", "conf" } );
+        ds.scan();
+
+        List<String> cp = new ArrayList<String>();
+
+        for ( String path : ds.getIncludedFiles() )
+        {
+            cp.add( path );
+        }
+
+        for ( String path : ds.getIncludedDirectories() )
+        {
+            cp.add( path );
+        }
+
+        return cp.toArray( new String[cp.size()] );
+    }
+
+    @Override
+    public String getProgramArguments( ILaunchConfiguration configuration )
+        throws CoreException
+    {
+        return "./conf/jetty.xml";
+    }
+
+    @Override
+    public String getVMArguments( ILaunchConfiguration configuration )
+        throws CoreException
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append( "-Djava.library.path=bin/jsw/lib " );
+        sb.append( super.getVMArguments( configuration ) );
+        return sb.toString();
+    }
 }
