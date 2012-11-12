256e10462e3cea3bcfdb2eb66ad0aef809722707
Igor Fedorenko <igor@ifedorenko.com>
2012-11-12 17:53:17 -0500

    wip: generate plugin-repository.xml
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- com.ifedorenko.m2e.nexusdev/src/com/ifedorenko/m2e/nexusdev/internal/NexusExternalLaunchDelegate.java
+++ com.ifedorenko.m2e.nexusdev/src/com/ifedorenko/m2e/nexusdev/internal/NexusExternalLaunchDelegate.java
@@ -1,10 +1,18 @@
 package com.ifedorenko.m2e.nexusdev.internal;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.codehaus.plexus.util.DirectoryScanner;
+import org.codehaus.plexus.util.WriterFactory;
+import org.codehaus.plexus.util.xml.XmlStreamWriter;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomWriter;
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
@@ -16,6 +24,8 @@ import org.eclipse.debug.core.sourcelookup.ISourceLookupParticipant;
 import org.eclipse.jdt.internal.launching.JavaSourceLookupDirector;
 import org.eclipse.jdt.launching.JavaLaunchDelegate;
 import org.eclipse.jdt.launching.sourcelookup.containers.JavaSourceLookupParticipant;
+import org.eclipse.m2e.core.MavenPlugin;
+import org.eclipse.m2e.core.project.IMavenProjectFacade;
 
 import com.ifedorenko.m2e.sourcelookup.internal.SourceLookupMavenLaunchParticipant;
 
@@ -61,6 +71,8 @@ public class NexusExternalLaunchDelegate
 
             launch.setSourceLocator( sourceLocator );
 
+            writePluginRepositoryXml( new File( getNexusWorkingDirectory(), "plugin-repository.xml" ) );
+
             super.launch( configuration, mode, launch, monitor );
         }
         finally
@@ -71,14 +83,20 @@ public class NexusExternalLaunchDelegate
         }
     }
 
+    private File getNexusWorkingDirectory()
+    {
+        // XXX
+        return new File( "/tmp/sonatype-work/nexus" );
+    }
+
     @Override
     public File verifyWorkingDirectory( ILaunchConfiguration configuration )
         throws CoreException
     {
-        return getInstallationDirectory( configuration );
+        return getNexusInstallationDirectory( configuration );
     }
 
-    private File getInstallationDirectory( ILaunchConfiguration configuration )
+    private File getNexusInstallationDirectory( ILaunchConfiguration configuration )
         throws CoreException
     {
         String location = configuration.getAttribute( ATTR_INSTALLATION_LOCATION, (String) null );
@@ -113,7 +131,7 @@ public class NexusExternalLaunchDelegate
     {
 
         DirectoryScanner ds = new DirectoryScanner();
-        ds.setBasedir( getInstallationDirectory( configuration ) );
+        ds.setBasedir( getNexusInstallationDirectory( configuration ) );
         ds.setIncludes( new String[] { "lib/*.jar", "conf" } );
         ds.scan();
 
@@ -149,6 +167,7 @@ public class NexusExternalLaunchDelegate
         {
             append( sb, sourcelookup.getVMArguments( configuration, launch, monitor ) );
         }
+        sb.append( " -Dnexus-work=" ).append( getNexusWorkingDirectory().getAbsolutePath() );
         return sb.toString();
     }
 
@@ -158,5 +177,61 @@ public class NexusExternalLaunchDelegate
         {
             sb.append( ' ' ).append( str );
         }
+    }
+
+    private void writePluginRepositoryXml( File pluginRepositoryXml )
+        throws CoreException
+    {
+        final IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
+
+        Xpp3Dom repositoryDom = new Xpp3Dom( "plugin-repository" );
+
+        Xpp3Dom artifactsDom = new Xpp3Dom( "artifacts" );
+        repositoryDom.addChild( artifactsDom );
+
+        for ( IMavenProjectFacade project : MavenPlugin.getMavenProjectRegistry().getProjects() )
+        {
+            IFolder output = root.getFolder( project.getOutputLocation() );
+            if ( "nexus-plugin".equals( project.getPackaging() ) && output.isAccessible() )
+            {
+                Xpp3Dom artifactDom = new Xpp3Dom( "artifact" );
+
+                addChild( artifactDom, "location", output.getLocation().toOSString() );
+
+                addChild( artifactDom, "groupId", project.getArtifactKey().getGroupId() );
+                addChild( artifactDom, "artifactId", project.getArtifactKey().getArtifactId() );
+                addChild( artifactDom, "version", project.getArtifactKey().getVersion() );
+                addChild( artifactDom, "groupId", project.getArtifactKey().getGroupId() );
+                addChild( artifactDom, "type", project.getPackaging() );
+
+                artifactsDom.addChild( artifactDom );
+            }
+        }
+
+        try
+        {
+            pluginRepositoryXml.getParentFile().mkdirs();
+            XmlStreamWriter writer = WriterFactory.newXmlWriter( pluginRepositoryXml );
+            try
+            {
+                Xpp3DomWriter.write( writer, repositoryDom );
+            }
+            finally
+            {
+                writer.close();
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new CoreException( new Status( IStatus.ERROR, NexusdevActivator.BUNDLE_ID,
+                                                 "Could not write nexus plugin-repository.xlm file", e ) );
+        }
+    }
+
+    private static void addChild( Xpp3Dom dom, String name, String value )
+    {
+        Xpp3Dom child = new Xpp3Dom( name );
+        child.setValue( value );
+        dom.addChild( child );
     }
 }
